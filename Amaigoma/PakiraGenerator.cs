09649bbbc58a4c6f1c66c85e41d603d561783274
David Rolland <rold2007@users.noreply.github.com>
2018-02-18 16:13:20 +0900

    Partial commit

---
+++ Amaigoma/PakiraGenerator.cs
@@ -0,0 +1,71 @@
+﻿namespace Amaigoma
+{
+   using System;
+   using numl;
+   using numl.Math;
+   using numl.Math.LinearAlgebra;
+   using numl.Model;
+   using numl.Supervised;
+   using numl.Supervised.NaiveBayes;
+
+   public class PakiraGenerator : Generator
+   {
+      public PakiraGenerator()
+      {
+      }
+
+      public override IModel Generate(Matrix x, Vector y)
+      {
+         if (Descriptor == null)
+            throw new InvalidOperationException("Cannot build naive bayes model without type knowledge!");
+
+         // create answer probabilities
+         if (!Descriptor.Label.Discrete)
+            throw new InvalidOperationException("Need to use regression for non-discrete labels!");
+
+         NaiveBayesModel naiveBayesModel = new NaiveBayesModel();
+
+         naiveBayesModel.Descriptor = this.Descriptor;
+
+         Statistic[] statistics = new Statistic[1];
+
+         statistics[0] = new Statistic();
+
+         statistics[0].Probability = 100.0;
+
+         statistics[0].Conditionals = new Measure[4];
+
+         for (int i = 0; i < x.Cols; i++)
+         {
+            statistics[0].Conditionals[i] = new Measure();
+
+            statistics[0].Conditionals[i].Probabilities = new Statistic[1];
+            statistics[0].Conditionals[i].Probabilities[0] = new Statistic();
+
+            statistics[0].Conditionals[i].Probabilities[0].X = new Range();
+
+            statistics[0].Conditionals[i].Probabilities[0].X.Min = 0.0;
+            statistics[0].Conditionals[i].Probabilities[0].X.Max = 0.0;
+
+            statistics[0].Conditionals[i].Probabilities[0].Conditionals = new Measure[1];
+            // statistics[0].Conditionals[i].Probabilities[0].Conditionals[0].
+         }
+
+         statistics[0].X = new Range();
+
+         statistics[0].X.Min = 0.0;
+         statistics[0].X.Max = 0.0;
+
+         Measure root = new Measure
+         {
+            Discrete = true,
+            Label = Descriptor.Label.Name,
+            Probabilities = statistics
+         };
+
+         naiveBayesModel.Root = root;
+
+         return naiveBayesModel;
+      }
+   }
+}
---
+++ AmaigomaConsole/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace AmaigomaConsole
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Text;
+   using System.Threading.Tasks;
+   using Amaigoma;
+   using numl;
+   using numl.Model;
+   using numl.Supervised;
+   using numl.Supervised.DecisionTree;
+   using numl.Supervised.NaiveBayes;
+   using numl.Tests.Data;
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Descriptor description = Descriptor.Create<Iris>();
+
+         Console.WriteLine(description);
+
+         Iris[] data = Iris.Load();
+         List<Generator> generators = new List<Generator>
+         {
+            //new PakiraGenerator(),
+            //new DecisionTreeGenerator(description),
+            new NaiveBayesGenerator(2),
+            new NaiveBayesGenerator(3),
+            new NaiveBayesGenerator(5),
+            new NaiveBayesGenerator(8),
+            new NaiveBayesGenerator(13),
+            new NaiveBayesGenerator(21)
+         };
+
+         foreach (Generator generator in generators)
+         {
+            IModel model = generator.Generate(description, data);
+
+            Iris prediction;
+
+            prediction = data[0];
+
+            //prediction = decisionTreeModel.Predict(prediction);
+            prediction = model.Predict(prediction);
+
+            prediction.Class = string.Empty;
+            prediction.PetalLength = 9.9m;
+            prediction.PetalWidth = 9.9m;
+            prediction.SepalLength = 9.9m;
+            prediction.SepalWidth = 9.9m;
+
+            //prediction = decisionTreeModel.Predict(prediction);
+            prediction = model.Predict(prediction);
+
+            prediction.Class = string.Empty;
+            prediction.PetalLength = 0.0m;
+            prediction.PetalWidth = 0.0m;
+            prediction.SepalLength = 0.0m;
+            prediction.SepalWidth = 0.0m;
+
+            //prediction = decisionTreeModel.Predict(prediction);
+            prediction = model.Predict(prediction);
+
+            prediction.Class = string.Empty;
+            prediction.PetalLength = 5.2m;
+            prediction.PetalWidth = 1.9m;
+            prediction.SepalLength = 6.0m;
+            prediction.SepalWidth = 3.1m;
+
+            //prediction = decisionTreeModel.Predict(prediction);
+            prediction = model.Predict(prediction);
+
+            //prediction = decisionTreeModel.Predict(data[149]);
+            prediction = model.Predict(data[149]);
+
+            LearningModel learned;
+            IModel learnedModel;
+            double accuracy;
+            //  learned = Learner.Learn(data, 0.80, 1, decisionTreeGenerator);
+            //  learnedModel = learned.Model;
+            //  accuracy = learned.Accuracy;
+
+            learned = Learner.Learn(data, 0.10, 10, generator);
+            learnedModel = learned.Model;
+            accuracy = learned.Accuracy;
+         }
+      }
+   }
+}
