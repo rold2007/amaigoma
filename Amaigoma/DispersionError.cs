ed342dfcdcc52b24b75309397fe2b2e6b42ae616
David Rolland <rold2007@users.noreply.github.com>
2019-02-17 21:15:34 +0900

    Added missing file.

---
+++ Amaigoma/DispersionError.cs
@@ -0,0 +1,17 @@
+﻿namespace Amaigoma
+{
+   using System;
+   using numl.Math.Information;
+   using numl.Math.LinearAlgebra;
+
+   public class DispersionError : Impurity
+   {
+      public override double Calculate(Vector x)
+      {
+         if (x == null)
+            throw new InvalidOperationException("x does not exist!");
+
+         return 0.0;
+      }
+   }
+}
