64a859c154152f275230409b2028feb9ce9935a1
David Rolland <rold2007@yahoo.com>
2019-06-02 22:09:51 +0900

    Started adding unit tests.

---
+++ AmaigomaTests/PakiraGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace AmaigomaTests
+{
+   using Amaigoma;
+   using System.Collections.Generic;
+   using Xunit;
+
+   public class PakiraGeneratorTests
+   {
+      [Fact]
+      public void Constructor()
+      {
+         PakiraGenerator pakiraGenerator = PakiraGeneratorTests.CreatePakiraGeneratorInstance();
+      }
+
+      [Fact]
+      public void Generate()
+      {
+         PakiraGenerator pakiraGenerator = PakiraGeneratorTests.CreatePakiraGeneratorInstance();
+         List<int[]> samples = new List<int[]>();
+
+         pakiraGenerator.Generate(samples);
+      }
+
+      static public PakiraGenerator CreatePakiraGeneratorInstance()
+      {
+         return new PakiraGenerator();
+      }
+   }
+}
